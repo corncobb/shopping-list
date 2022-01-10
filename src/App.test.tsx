c5ac3acb8753beb3123bf99c9eba2f2255a730f9
corncobb <cameronjcobb@gmail.com>
2022-01-10 01:06:00 -0800

    add meta data to index.html

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,7 @@ import { render, screen } from "@testing-library/react";
 import React from "react";
 import { App } from "./App";
 
+// Not writing a test for this
 test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
